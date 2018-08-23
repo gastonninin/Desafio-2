cf69ef903c2ddbaacd751296559f79044a92970f
“gastonninin” <gastonninin@gmail.com>
2018-08-23 14:04:23 -0300

    Desafio 2

---
+++ index.js
@@ -0,0 +1,142 @@
+
+/*
+var name; // declaracion
+var age = 24;
+var isActive;
+
+name = 'Gaston'; // asignacion
+age = 25; //number
+km = 30; // float
+distance = 5; 
+isActive = true;
+
+console.log('Mi nombre es ' + name + 
+		' y tengo ' + age + 
+		' años ' + 
+		'vivo a ' + km + 
+		' km de distancia y recorri ' +  distance  + ' cuadras desde la parada de colectivo ');
+
+
+var name = 'Gaston';
+var number = 6;
+var year = 1993;
+var mes = ['enero', 'febrero', 
+				'marzo', 'abril', 'mayo', 'junio', 
+				'julio', 'agosto', 'septiembre', 
+				'octubre', 'noviembre', 'diciembre'];
+
+console.log(name + ' nació el ' + number + ' de ' + mes [7] + ' de ' + year)
+*/
+
+/*
+var person = {
+	name : 'Gaston',
+	age : 25,
+};
+
+var persons = new Array();
+persons.push({name : 'Gaston',age : 28});
+persons.push({name : 'Tom', age : 18});
+
+console.log(persons)
+*/
+
+/*
+var person = {
+	name : 'Gaston',
+	age: 25,
+	distance: 30,
+	day: 6,
+	mes: ['enero', 'febrero', 
+				'marzo', 'abril', 'mayo', 'junio', 
+				'julio', 'agosto', 'septiembre', 
+				'octubre', 'noviembre', 'diciembre'],
+	year: 1993
+}
+
+console.log('Mi nombre es ' + person.name + ' tengo ' 
+				+ person.age + ' años ' + ' vivo a ' + person.distance + ' kilometros '
+				+ ' y nací el ' + person.day + ' de ' + person.mes[7] + ' de ' + person.year)
+*/
+
+/*
+var num1 = 100;
+var num2 = 50;
+
+var sum = num1 + num2;
+
+console.log(num1+num2)
+console.log("el resultado es " + (num1+num2))
+console.log("el resultado es " + (num1-num2))
+console.log("el resultado es " + (num1/num2))
+console.log("el resultado es " + (num1*num2))
+console.log("el resultado es " + (num1%num2))
+
+/*num1--;
+
+console.log("el resultado es " + num1)*/
+
+/*
+num1+= 2;
+
+console.log("el resultado es " + num1)
+*/
+
+
+
+
+var uruguay = prompt("¿Quién fue el primer campeón del mundo?")
+if (uruguay === "Uruguay") {
+	alert("El primer campeón del mundo fue " + uruguay);
+} else {
+	alert("Respuesta incorrecta");
+}
+
+var gremio = prompt("¿Quién es el último campeón de la Copa Libertadores?")
+if (gremio === "Gremio") {
+ 	alert("El último campeón de la Copa Libertadores es " + gremio);
+ } else {
+ 	alert("Respuesta incorrecta");
+ }
+
+var zarate = prompt("¿Dónde jugaba Mauro Zárate antes de llegar a Boca?")
+if (zarate === "Vélez") {
+ 	alert("Antes de llegar a Boca, Mauro Zárate jugaba en " + zarate);
+ } else {
+ 	alert("Respuesta incorrecta");
+ }
+
+var VAR = prompt("¿En qué Mundial se implementó por primera vez el VAR?")
+if (VAR === "Rusia 2018") {
+ 	alert("El primer Mundial donde se implementó el VAR fue en " + VAR);
+ } else {
+ 	alert("Respuesta incorrecta");
+ }
+
+
+
+
+ 
+
+ 
+
+
+
+
+/*
+console.log("El primer campeón del mundo fue " + uruguay);
+console.log("El último campeón de la Copa Libertadores es " + gremio)
+console.log("Antes de llegar a Boca, Mauro Zárate jugaba en " + zarate)
+console.log("El VAR se implementó por primera vez en " + VAR)
+
+
+
+var num1 = prompt("Escribe un número");
+var num2 = prompt("Escribe un número");
+
+var res = parseInt(num1) + parseInt(num2)
+var res1 = parseInt(num1) - parseInt(num2)
+
+console.log("El resultado de " + num1 + " + " + num2 + " es " + res)
+console.log("El resultado de " + num1 + " - " + num2 + " es " + res1)
+*/
